bcaed0644f7cf66f6eae6525c704447eb054c5a2
kalki7 <anamsaatvik000@gmail.com>
2019-12-18 20:00:00 +0530

    Listing Added, Team removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListingComponent } from './listing/listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    AboutComponent,
+    ServicesComponent,
+    TeamComponent,
+    ContactComponent,
+    FooterComponent,
+    ListingComponent
   ],
   imports: [
     BrowserModule
